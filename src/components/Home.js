b8336f7f182fcfe8349e570ab6895870272ebe84
muhmdpp <connect.mhdpp@gmail.com>
2024-04-06 20:26:46 +0530

    home-font-color-change

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <div className='home-banner'>
         <div className='top-con'>
           <h5 className="text-white bg-pink-800 mt-5 text-center h-11 w-auto pt-2 font-bold">HOME PAGE</h5>
-          <h3>Unleash Your Brilliance,<br /> Embrace Education</h3>
+          <h3 className='text-black'>Unleash Your Brilliance,<br /> Embrace Education</h3>
           <button className='view-btn'>view more</button>
         </div>
       </div>
